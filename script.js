e3b362a4f6dcf65f3f41772ac31f753d0e0c8f3f
Horakhsh Sharifi <horakhshsharifi1@gmail.com>
2023-09-24 13:25:59 +0500

    sixth commit

--- script.js
+++ script.js
@@ -291,7 +291,7 @@ const carousel = document.querySelector(".carousel"),
   picturePercentge = document.querySelector("#portfolio-footer-percentge");
 arrowIcons.forEach((icon) => {
   icon.addEventListener("click", () => {
-    let firstImgWidth = firstImg.clientWidth + 14;
+    let firstImgWidth = firstImg.clientWidth + 20;
     carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
   });
 });
